8e9f6d4f29a71ac54bef21e8936659174de137f3
Yuri Sanchez <yuri.tecnologia@outlook.com>
2026-07-10 14:44:30 -0300

    refactor: retirado imports que nao estavam sendo usados

--- src/main/java/movieflix/Controller/CategoryController.java
+++ src/main/java/movieflix/Controller/CategoryController.java
@@ -2,8 +2,6 @@ package movieflix.Controller;
 
 import lombok.RequiredArgsConstructor;
 import movieflix.Controller.response.CategoryResponse;
-import movieflix.Entity.Category;
-import movieflix.Mapper.CategoryMapper;
 import movieflix.Service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
