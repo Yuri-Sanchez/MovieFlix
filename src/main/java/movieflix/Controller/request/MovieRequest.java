07dffc2ce7e5437a092b711e1597481a84bc00c4
Yuri Sanchez <yuri.tecnologia@outlook.com>
2026-07-23 18:12:07 -0300

    refactor: altera formato da data para padrão brasileiro nos DTOs

--- src/main/java/movieflix/Controller/request/MovieRequest.java
+++ src/main/java/movieflix/Controller/request/MovieRequest.java
@@ -1,10 +1,14 @@
 package movieflix.Controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record MovieRequest(String name,
                            String description,
+
+                           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
                            double rating,
                            List<Long> categories,
